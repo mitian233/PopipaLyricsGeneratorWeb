98d1688163e6ab08e1d797622d474fce000e9c07
Aaki阿湫 <mitian233@yahoo.co.jp>
2021-09-10 13:43:11 +0800

    update

--- js/main.js
+++ js/main.js
@@ -32,31 +32,31 @@ function gen(input_id, gen_id) {
   var gen_times = document.getElementById(input_id).value //取文本输入框值
   console.log(gen_times);
   console.log(isNaN(gen_times));
-  console.log(isnull(document.getElementById(input_id).value))
+  console.log(isnull(document.getElementById(input_id).value));
+  var result = "";
   if (isNaN(gen_times) == true || isnull(gen_times) == true || (gen_times <= 0) == true) {//检测是否是非数值型
-    mdui.alert('Check input');
+    mdui.alert('Invalid Input. Check again. ');
   } else {
-    var result="";
     var i;
     for (i = 0; i < gen_times; i++) {
-      var result;
       var have_ni = randomNum(0, 1);
       if (have_ni == 0) {
         var m_range = randomNum(1, 13);
         var f_range = randomNum(0, 1);
         var d_range = randomNum(0, 3);
         var j_range = randomNum(0, 4);
-        result += meishi[m_range] + jyoushi[j_range] + doushi[d_range] + "\n";
+        result += (meishi[m_range] + jyoushi[j_range] + doushi[d_range] + '\n');
       } else {
         var m_range = randomNum(1, 13);
         var f_range = randomNum(0, 1);
         var d_range = randomNum(1, 3);
         var j_range = randomNum(0, 4);
-        result += meishi[m_range] + jyoushi[j_range] + fukushi[1] + 'に' + doushi[d_range] + "\n";
+        result += (meishi[m_range] + jyoushi[j_range] + fukushi[1] + 'に' + doushi[d_range] + '\n');
       }
     }
+    ;
+    document.getElementById(gen_id).rows = (Number(gen_times)+1);
+    console.log(result);
+    document.getElementById(gen_id).value = result;//输出到文本对象
   }
-  ;
-  console.log('have_ni:' + bool[have_ni] + ' m:' + meishi[m_range] + ' d_range:' + doushi[d_range] + ' j_range:' + jyoushi[j_range]);
-  document.getElementById(gen_id).innerText = result;//输出到文本对象
 };
