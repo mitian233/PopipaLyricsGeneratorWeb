5e1b6a27027ad780336bbe871fc3b96d6a9a3a0f
Aaki阿湫 <mitian233@yahoo.co.jp>
2021-09-18 10:43:23 +0800

    dict improve

--- js/main.js
+++ js/main.js
@@ -30,33 +30,52 @@ function isnull(val) {
 
 function gen(input_id, gen_id) {
   var gen_times = document.getElementById(input_id).value //取文本输入框值
-  console.log(gen_times);
-  console.log(isNaN(gen_times));
-  console.log(isnull(document.getElementById(input_id).value));
   var result = "";
   if (isNaN(gen_times) == true || isnull(gen_times) == true || (gen_times <= 0) == true) {//检测是否是非数值型
     mdui.alert('Invalid Input. Check again. ');
   } else {
     var i;
     for (i = 0; i < gen_times; i++) {
-      var have_ni = randomNum(0, 1);
-      if (have_ni == 0) {
-        var m_range = randomNum(1, 13);
-        var f_range = randomNum(0, 1);
-        var d_range = randomNum(0, 3);
-        var j_range = randomNum(0, 4);
-        result += (meishi[m_range] + jyoushi[j_range] + doushi[d_range] + '\n');
-      } else {
-        var m_range = randomNum(1, 13);
-        var f_range = randomNum(0, 1);
-        var d_range = randomNum(1, 3);
-        var j_range = randomNum(0, 4);
-        result += (meishi[m_range] + jyoushi[j_range] + fukushi[1] + 'に' + doushi[d_range] + '\n');
+      var sentence_type = randomNum(0, 4);
+      switch (sentence_type) {
+        case 0:
+          var m_range = randomNum(1, 357);
+          var d_range = randomNum(0, 393);
+          var j_range = randomNum(0, 4);
+          result += (meishi[m_range] + jyoushi[j_range] + doushi[d_range] + '\n');
+          break;
+        case 1:
+          var m_range = randomNum(1, 357);
+          var f_range = randomNum(1, 24);
+          var d_range = randomNum(1, 393);
+          var j_range = randomNum(0, 4);
+          result += (meishi[m_range] + jyoushi[j_range] + fukushi[f_range] + 'に' + doushi[d_range] + '\n');
+          break;
+        case 2:
+          var m_range = randomNum(1, 357);
+          var dt_range = randomNum(1, 393);
+          var d_range = randomNum(1, 393);
+          var j_range = randomNum(0, 4);
+          result += (doushi_te[dt_range] + meishi[m_range] + jyoushi[j_range] + doushi[d_range] + '\n');
+          break;
+        case 3:
+          var dm_range = randomNum(1, 28);
+          var j_range = randomNum(0, 5);
+          var f_range = randomNum(1, 24);
+          var d_range = randomNum(1, 393);
+          result += (daimeishi[dm_range] + jyoushi[j_range] + fukushi[f_range] + 'に' + doushi[d_range] + '\n')
+        default:
+          var i1;
+          var rantimes = randomNum(1, 4);
+          for (i1 = 0; i1 < rantimes; i1++) {
+            var mc_range = randomNum(1, 448);
+            result += (michi[mc_range]+' ');
+          };
+          result += ('\n');
+          break;
       }
-    }
-    ;
-    document.getElementById(gen_id).rows = (Number(gen_times)+1);
-    console.log(result);
+    };
+    document.getElementById(gen_id).rows = (Number(gen_times) + 1);
     document.getElementById(gen_id).value = result;//输出到文本对象
   }
 };
